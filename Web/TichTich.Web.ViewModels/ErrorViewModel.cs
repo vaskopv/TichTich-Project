f9cd9c5fd413c1f3a99e8320f99e7a128fbd6ce2
Vasil <vasko_v@abv.bg>
2020-04-22 07:26:01 +0300

    Added Error Page

--- Web/TichTich.Web.ViewModels/ErrorViewModel.cs
+++ Web/TichTich.Web.ViewModels/ErrorViewModel.cs
@@ -2,6 +2,9 @@
 {
     public class ErrorViewModel
     {
+        public int StatusCode { get; set; }
+
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
